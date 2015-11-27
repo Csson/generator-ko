4e084c878c1f9217405397bbdaa91aa48d6040a5
Erik Carlsson <info@code301.com>
2015-11-27 16:28:50 +0000

    Allow directories in component names. When directories are used, sub-directories are not created.

--- component/index.js
+++ component/index.js
@@ -12,32 +12,36 @@ var ComponentGenerator = yeoman.generators.NamedBase.extend({
 
   init: function () {
     var codeLanguage = 'JavaScript';
-    console.log('Creating component \'' + this.name + '\' (' + codeLanguage + ')...');
-    this.componentName = this.name;
-    this.dirname = 'src/components/' + this._.dasherize(this.name) + '/';
-    this.filename = this._.dasherize(this.name);
+
+    var dasherized_name = this._.dasherize(this.name);
+    this.componentName = dasherized_name.replace(/\//g, '-');
+    this.dirname = dasherized_name.replace(/\/[^/]*$/, '') + '/';
+    this.fulldirname = 'src/components/' + this.dirname;
+    this.filename = dasherized_name.replace(/^.*\//, '');
+    this.dashname = dasherized_name;
     this.viewModelClassName = this._.classify(this.name);
+    console.log('Creating component \'' + this.componentName + '\' in <' + this.dirname + '> (' + codeLanguage + ')...');
   },
 
   template: function () {
     var codeExtension = '.js';
-    this.copy('view.html', this.dirname + this.filename + '.html');
-    this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.filename + this.codeFileExtension);
+    this.copy('view.html', this.fulldirname + this.filename + '.html');
+    this.copy('viewmodel' + this.codeFileExtension, this.fulldirname + this.filename + this.codeFileExtension);
   },
 
   addComponentRegistration: function() {
     var startupFile = 'src/app/startup' + this.codeFileExtension;
     readIfFileExists.call(this, startupFile, function(existingContents) {
-        var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
+        var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.componentName + '[\'"]');
         if (existingRegistrationRegex.exec(existingContents)) {
-            this.log(chalk.white(this.filename) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
+            this.log(chalk.white(this.dashname) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
             return;
         }
 
         var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]',
             regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
-            modulePath = 'components/' + this.filename + '/' + this.filename,
-            lineToAdd = 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
+            modulePath = 'components/' + this.dirname + this.filename,
+            lineToAdd = 'ko.components.register(\'' + this.componentName + '\', { require: \'' + modulePath + '\' });',
             newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
         fs.writeFile(startupFile, newContents);
         this.log(chalk.green('   registered ') + chalk.white(this.filename) + chalk.green(' in ') + chalk.white(startupFile));
